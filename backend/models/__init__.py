35f1c3941850957151d928bf7c9e52d3032da76d
glitchzeros <standoffakk482@gmail.com>
2025-06-08 23:48:51 +0500

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,36 +3,39 @@ Database models for Aetherium
 The Scribe's Data Structures
 """
 
-# Temporarily import only essential models to avoid relationship issues
+# Import in an order that respects dependencies
 from .subscription import SubscriptionTier
-# from .user import User
-# from .subscription import UserSubscription
-# from .workflow import ScribeWorkflow
-# from .session import CommunicationSession, SessionMessage
-# from .statistics import CallStatistics
-# from .modem import GSMModem, SMSMessage
-# from .payment import PaymentTransaction
-# from .telegram import TelegramChat
-# from .dream_journal import ScribeDreamJournal
-# from .company_number import CompanyNumberPool
-# from .gsm_module import GSMModule, PaymentSession
-# from .sms_verification import SMSVerificationSession
+from .user import User
+from .subscription import UserSubscription
+from .workflow import ScribeWorkflow
+from .session import CommunicationSession, SessionMessage
+from .statistics import CallStatistics
+from .modem import GSMModem, SMSMessage
+from .payment import PaymentTransaction, ManualPaymentSession
+from .telegram import TelegramChat
+from .dream_journal import ScribeDreamJournal
+from .company_number import CompanyNumberPool
+from .gsm_module import GSMModule, PaymentSession
+from .sms_verification import SMSVerificationSession, PaymentMonitoringSession, GSMModuleManagement
 
 __all__ = [
     "SubscriptionTier",
-    # "User",
-    # "UserSubscription", 
-    # "ScribeWorkflow",
-    # "CommunicationSession",
-    # "SessionMessage",
-    # "CallStatistics",
-    # "GSMModem",
-    # "SMSMessage",
-    # "PaymentTransaction",
-    # "TelegramChat",
-    # "ScribeDreamJournal",
-    # "CompanyNumberPool",
-    # "GSMModule",
-    # "PaymentSession",
-    # "SMSVerificationSession"
+    "User",
+    "UserSubscription", 
+    "ScribeWorkflow",
+    "CommunicationSession",
+    "SessionMessage",
+    "CallStatistics",
+    "GSMModem",
+    "SMSMessage",
+    "PaymentTransaction",
+    "ManualPaymentSession",
+    "TelegramChat",
+    "ScribeDreamJournal",
+    "CompanyNumberPool",
+    "GSMModule",
+    "PaymentSession",
+    "SMSVerificationSession",
+    "PaymentMonitoringSession",
+    "GSMModuleManagement"
 ]
